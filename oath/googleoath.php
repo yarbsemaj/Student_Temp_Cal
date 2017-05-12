d04cac3aeb206fb76ccd8d1d249d2b6403d832f3
yarbsemaj <yarbsemaj@gmail.com>
2017-05-12 02:16:02 +0100

    Added fave icon, Fine tuning

--- oath/googleoath.php
+++ oath/googleoath.php
@@ -14,6 +14,6 @@ $client->addScope(Google_Service_Calendar::CALENDAR);
 $client->addScope(Google_Service_Plus::USERINFO_PROFILE);
 $client->addScope(Google_Service_Oauth2::USERINFO_EMAIL);
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oath/oa2cb.php');
-$client->setPrompt("consent");
+
 $auth_url = $client->createAuthUrl();
 header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
